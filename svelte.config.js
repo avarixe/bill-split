eb512beb8368bc9864bedc4406370dd10252d5a3
Joon Lee <l33j00n325@gmail.com>
2021-09-12 23:56:48 -0700

    fix static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,11 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
     // paths: { // uncomment when deploying to GitHub
     //   base: '/bill-split'
     // },
